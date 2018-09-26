43a3f5b5a62cbe838bd0bbd1ca2a6b188ffb3443
jay <jay@jay.local>
2018-09-26 11:37:44 +0800

    日志工具修改

--- log_util.py
+++ log_util.py
@@ -4,12 +4,13 @@ import logging
 
 
 #---------------------中文日志
+import logging
 logging.basicConfig(level=logging.WARNING, filemode='a', datefmt='"%Y/%m/%d/ %H:%M:%S "',
                     format='%(asctime)s -- %(message)s', )
 encoding = "UTF-8"
 handler = logging.FileHandler(filename='mylog.log', encoding='utf-8')
-fmt = logging.Formatter(fmt='%(asctime)s -- %(message)s',datefmt='"%Y/%m/%d/ %H:%M:%S "')
+fmt = logging.Formatter(fmt='%(asctime)s -- %(message)s', datefmt='"%Y/%m/%d/ %H:%M:%S "')
 handler.setFormatter(fmt)
-logging.getLogger().addHandler(handler)
-
-logging.error('测试')
+grid_log = logging.getLogger('grid_log')
+grid_log.addHandler(handler)
+grid_log.error('测试')
